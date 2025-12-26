1922892b2ff7264228613315e60e830a10126e2f
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2025-12-25 19:50:37 -0500

    fixed volume sliders, added settings icon ingame

--- giftbox-project-insides/scenes/audio_control_music.gd
+++ giftbox-project-insides/scenes/audio_control_music.gd
@@ -2,11 +2,12 @@ extends HSlider
 
 @export var audio_bus_name: String
 
-var audio_bus_id
+var audio_bus_id: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
-
+	var db = AudioServer.get_bus_volume_db(audio_bus_id)
+	value = db_to_linear(db)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ giftbox-project-insides/scripts/ui.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@onready var settings_icon = $SettingsIcon
+@onready var settings_panel = $Settings
+@onready var dimmer = $ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	settings_panel.visible = false
+	settings_icon.visible = true
+	dimmer.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_texture_button_pressed() -> void:
+	settings_panel.visible = true
+	settings_icon.visible = false
+	dimmer.visible = true
